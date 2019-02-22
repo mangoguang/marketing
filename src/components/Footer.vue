f29409347d528743f3e2e53329bf480cca7170b4
xiaowei <578850593@qq.com>
2019-02-22 11:20:07 +0800

    更改路径

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -26,7 +26,7 @@
       </router-link>
     </li>
     <li id="4" >
-      <router-link to="/msmanage" >
+      <router-link to="/msManage" @click.native="changeActive">
        <div class="iconMsg" ></div>
       <p class="iconCharP">消息</p>
       </router-link>
