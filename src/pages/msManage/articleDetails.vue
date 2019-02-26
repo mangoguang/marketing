4fa93adc4f41e2959e58b11c6d76158cba02d8bc
xiaowei <578850593@qq.com>
2019-02-26 11:19:55 +0800

    文章详情vedio未完成

--- src/pages/msManage/articleDetails.vue
+++ src/pages/msManage/articleDetails.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="article">
-    <h1>{{ this.$route.query.title }}</h1>
-    <span>歌迪亚 2018.11.11</span>
-    <div class="section">
+  <div class="article paddingTop">
+    <banner :title="'文章详情'"/>
+    <div class="titleBar">
+      <h1>{{ this.$route.query.title }}</h1>
+      <span>歌迪亚 2018.11.11</span>
+      <collect-btn class="collentBtn" 
+        :isCollect='isCollect'
+        v-on:click.native='collect'/>
+    </div>
+    <div class="content">
       <h2>  (一)床垫的主要介绍</h2>
       <p>1.一线弹簧</p>
       <img src="../../assets/imgs/article_search.png" alt="">
@@ -12,14 +18,20 @@
 </template>
 
 <script>
-import Vuex, { mapMutations, mapState } from 'vuex'
+import Banner from '../../components/banner'
+import CollectBtn from '../../components/msManage/eggCollectBtn'
 export default {
+  components: {CollectBtn,Banner},
   data() {
     return {
-
+      isCollect: true
     }
   },
-  created() {
+  methods: {
+    // 切换收藏按钮
+    collect() {
+      this.isCollect = !this.isCollect
+    }
   }
 }
 </script>
@@ -28,15 +40,24 @@ export default {
 .article {
   padding: 4.26vw;
   text-align: center;
-  h1 {
-    color: #353535;
-    font-size: 4.53vw;
+  padding-top: 22vw;
+  .titleBar {
+    position: relative;
+    h1 {
+      color: #353535;
+      font-size: 4.53vw;
+    }
+    span {
+      color: #909090;
+      font-size: 3.73vw;
+    }
+    .collentBtn {
+      position: absolute;
+      right: 0;
+      top: 0
+    }
   }
-  span {
-    color: #909090;
-    font-size: 3.73vw;
-  }
-  .section {
+  .content {
     text-align: left;
     h2,p {
       color: #363636;
