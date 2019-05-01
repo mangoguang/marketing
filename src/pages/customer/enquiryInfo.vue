90af4e91f220518a4e29b4d3699fe082b57d2275
lincy <lincy@163.com>
2019-05-01 18:03:16 +0800

    修改传递phone

--- src/pages/customer/enquiryInfo.vue
+++ src/pages/customer/enquiryInfo.vue
@@ -32,7 +32,7 @@
       </div>
     </div>
     <!-- 意向信息-->
-    <intentionMsg v-show="dealTabStatus[2].status" :list='list.opportunityList'/>
+    <intentionMsg v-show="dealTabStatus[2].status" :list='list.opportunityList' :phone="phone"/>
     <!-- <EnquiryOrderInfo v-show="dealTabStatus[1].status"/> -->
     <!-- <div class="line"></div> -->
     <!-- <orderInfoDetails/> -->
