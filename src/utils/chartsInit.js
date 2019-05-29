c73e664ade2a84f99ee8ae04fffc0ddc1a7d793e
lincy <lincy@163.com>
2019-05-29 10:16:51 +0800

    调整客户来源报表重合

--- src/utils/chartsInit.js
+++ src/utils/chartsInit.js
@@ -28,7 +28,7 @@ export {emptyPieData}
 // salesVal标记是否为销售额，主要用于改变数据单位
 // routeTo图表点击事件，可不传
 // chartsIndex如果是循环渲染图表，则传图表下标
-chartsInit = (_this, chartName, vertical, salesVal, title, routeTo, chartsIndex,radio) => {
+chartsInit = (_this, chartName, vertical, salesVal, title, routeTo, chartsIndex,radio,source) => {
   let [dom, chartsData] = [_this.$refs[`${chartName}Container`], _this[`${chartName}Data`]]
   // console.log('传入charts插件的数据：', chartsData)
   if (typeof(chartsIndex) === 'number') {
@@ -38,8 +38,14 @@ chartsInit = (_this, chartName, vertical, salesVal, title, routeTo, chartsIndex,
   let series = chartsData.series
   if (dom) {
     if (series) {
-      dom.style.width = `100vw`      
-      dom.style.height = `${vertical === 'horizontal' ? (series[0].data.length * (window.innerWidth/100) * 3) + 100/(window.innerWidth/100) : 100}vw`      
+      dom.style.width = `100vw`  
+      if(source){
+        dom.style.height = `${vertical === 'horizontal' ? (series[0].data.length * (window.innerWidth/100) * 10) + 100/(window.innerWidth/100) : 100}vw`
+      }else{
+        dom.style.height = `${vertical === 'horizontal' ? (series[0].data.length * (window.innerWidth/100) * 3) + 100/(window.innerWidth/100) : 100}vw`
+      }
+            
+      // dom.style.minHeight = `1000vw`    
     } else {
       dom.style.height = `100vw`
       dom.style.width = `100vw`      
