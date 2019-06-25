0bba52fd2e0db5adc12aa4366fbdfa0cc259cef6
xiaowei <578850593@qq.com>
2019-06-25 11:18:56 +0800

    4s首页

---
+++ src/components/4s/index/contentBox.vue
@@ -0,0 +1,48 @@
+<!--  -->
+<template>
+  <div class="contentBox">
+    <h1>{{title}}</h1>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['title'],
+  data () {
+    return {
+
+    };
+  }
+}
+</script>
+<style lang='scss' scoped>
+.contentBox {
+  width: 89.86vw;
+  min-height: 34.13vw;
+  background: #fff;
+  border-radius: 1.33vw;
+  h1 {
+    font-weight: normal;
+    color: #666;
+    line-height: 10.66vw;
+    font-size: 3.7vw;
+    padding-left: 2.4vw;
+    position: relative;
+    box-sizing: border-box;
+  }
+  h1::after {
+    content: '';
+      position:absolute;
+      left:0;
+      bottom:0;
+      width:100%;
+      height:1px;
+      background: #ededed;
+      transform-origin:0 0;
+      transform:scaleY(0.5)
+    }
+}
+</style>
--- src/pages/4S/index/index.vue
+++ src/pages/4S/index/index.vue
@@ -1,10 +1,19 @@
 <!--  -->
 <template>
-  <div class="index">index4s</div>
+  <div class="index">
+    <div>头部</div>
+    <div>门店选择下拉框</div>
+    <contentBox class="box" :title="'日常检查'">星级检查</contentBox>
+    <contentBox class="box" :title="'评分报表'">检查记录</contentBox>
+  </div>
 </template>
 
 <script>
+import contentBox from '../../../components/4s/index/contentBox'
 export default {
+  components : {
+    contentBox
+  },
   data () {
     return {
 
@@ -13,5 +22,11 @@ export default {
 }
 </script>
 <style lang='scss' scoped>
-
+.index {
+  width: 100vw;
+  .box {
+    margin: 0 auto;
+    margin-bottom: 4.4vw;
+  }
+}
 </style>
