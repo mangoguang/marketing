dfb8554924ed5da100b96c7f091f7c22b8385c24
lincy <lincy@163.com>
2019-08-01 10:45:25 +0800

    修改保存图片提示的位置

--- src/pages/Gallery/preShare.vue
+++ src/pages/Gallery/preShare.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="share">
     <Banner :title="'产品分享'"/>
-    <div class="saveTips" v-show="successSave">保存成功</div>
+    <div class="saveTips" v-show="successSave" :style="{top:top}">保存成功</div>
     <div class="canvas" ref="creatImg">
       <div class="imgBg">
         <img src="../../assets/imgs/shareBg.png" alt="">
@@ -61,7 +61,8 @@ export default {
       imageData: "",
       pageUrl: "",
       test: "",
-      changeUrl: ""
+      changeUrl: "",
+      top:''
     };
   },
   computed: {
@@ -70,6 +71,7 @@ export default {
     })
   },
   created() {
+    this.isIphoneX()
     this.msg = this.$route.query.list;
     if(baseUrl==='https://op.derucci.com'){
       this.pageUrl = baseUrl +"/web/marketing/#/productDetails?id=" +this.msg.id +"&musi=1";
@@ -81,6 +83,15 @@ export default {
     this.getCode();
   },
   methods: {
+    isIphoneX(){
+      let phone = this.phoneSize()
+      console.log(phone)
+      if(phone === 'iphonex'){
+        this.top="21.466vw"
+      }else{
+        this.top="16.466vw"
+      }
+    },
     //生成二维码
     getCode() {
       let qrcode = new QRCode("qrcode", {
